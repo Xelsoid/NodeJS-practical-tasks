37f85e7fa85057b1cadd9111a6ea3fd82e5df7fc
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2023-09-23 19:44:46 +0500

    test: added unit tests for service

--- src/services/public-holidays.service.test.ts
+++ src/services/public-holidays.service.test.ts
@@ -1,115 +1,125 @@
-import {getListOfPublicHolidays, checkIfTodayIsPublicHoliday, getNextPublicHolidays} from "./public-holidays.service";
-import holidaysMock from './mockResponse.json';
-import holidaysFormatted from './mockFormattedResponse.json';
+import {
+  getListOfPublicHolidays,
+  checkIfTodayIsPublicHoliday,
+  getNextPublicHolidays,
+} from "./public-holidays.service";
+import holidaysMock from "./mockResponse.json";
+import holidaysFormatted from "./mockFormattedResponse.json";
 import axios from "axios";
-import {shortenPublicHoliday, validateInput} from "../helpers";
-import {PUBLIC_HOLIDAYS_API_URL} from "../config";
+import { shortenPublicHoliday, validateInput } from "../helpers";
+import { PUBLIC_HOLIDAYS_API_URL } from "../config";
 
-jest.mock('../helpers', () => ({
+jest.mock("../helpers", () => ({
   validateInput: jest.fn(() => true),
-  shortenPublicHoliday: jest.fn((data) => data)
-}))
+  shortenPublicHoliday: jest.fn((data) => data),
+}));
 
-describe('Servises tests', () => {
-  describe('getListOfPublicHolidays tests', () => {
+describe("Servises tests", () => {
+  describe("getListOfPublicHolidays tests", () => {
     afterEach(() => {
       jest.clearAllMocks();
     });
 
-    it('should trigger call with expected params', async () => {
+    it("should trigger call with expected params", async () => {
       const year = 2023;
       const country = "GB";
-      const axiosGetSpy = jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.resolve({ data: holidaysMock })
-      )
-      const genderResponse = await getListOfPublicHolidays( year,  country);
-      expect(axiosGetSpy).toHaveBeenCalledWith(`${PUBLIC_HOLIDAYS_API_URL}/PublicHolidays/${year}/${country}`)
+      const axiosGetSpy = jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.resolve({ data: holidaysMock }));
+      const genderResponse = await getListOfPublicHolidays(year, country);
+      expect(axiosGetSpy).toHaveBeenCalledWith(
+        `${PUBLIC_HOLIDAYS_API_URL}/PublicHolidays/${year}/${country}`,
+      );
     });
 
-    it('should simulate call and return data', async () => {
-      jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.resolve({ data: holidaysMock })
-      )
-      const genderResponse = await getListOfPublicHolidays( 2023,  "GB");
-      expect(genderResponse).toEqual(holidaysMock)
+    it("should simulate call and return data", async () => {
+      jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.resolve({ data: holidaysMock }));
+      const genderResponse = await getListOfPublicHolidays(2023, "GB");
+      expect(genderResponse).toEqual(holidaysMock);
     });
 
-    it('should simulate call and throw exception', async () => {
-      jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.reject(new Error('some error'))
-      )
-      const genderResponse = await getListOfPublicHolidays( 2023,  "GB");
-      expect(genderResponse).toEqual([])
+    it("should simulate call and throw exception", async () => {
+      jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.reject(new Error("some error")));
+      const genderResponse = await getListOfPublicHolidays(2023, "GB");
+      expect(genderResponse).toEqual([]);
     });
-  })
+  });
 
-  describe('checkIfTodayIsPublicHoliday tests', () => {
+  describe("checkIfTodayIsPublicHoliday tests", () => {
     afterEach(() => {
       jest.clearAllMocks();
     });
 
-    it('should trigger call with expected params', async () => {
+    it("should trigger call with expected params", async () => {
       const country = "GB";
-      const axiosGetSpy = jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.resolve({ data: holidaysMock })
-      )
+      const axiosGetSpy = jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.resolve({ data: holidaysMock }));
       const genderResponse = await checkIfTodayIsPublicHoliday(country);
-      expect(axiosGetSpy).toHaveBeenCalledWith(`${PUBLIC_HOLIDAYS_API_URL}/IsTodayPublicHoliday/${country}`)
+      expect(axiosGetSpy).toHaveBeenCalledWith(
+        `${PUBLIC_HOLIDAYS_API_URL}/IsTodayPublicHoliday/${country}`,
+      );
     });
 
-    it('should simulate call and return 200 status', async () => {
-      jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.resolve({ status: 200 })
-      )
+    it("should simulate call and return 200 status", async () => {
+      jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.resolve({ status: 200 }));
       const genderResponse = await checkIfTodayIsPublicHoliday("GB");
-      expect(genderResponse).toEqual(true)
+      expect(genderResponse).toEqual(true);
     });
 
-    it('should simulate call and return 400 status', async () => {
-      jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.resolve({ status: 400 })
-      )
+    it("should simulate call and return 400 status", async () => {
+      jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.resolve({ status: 400 }));
       const genderResponse = await checkIfTodayIsPublicHoliday("GB");
-      expect(genderResponse).toEqual(false)
+      expect(genderResponse).toEqual(false);
     });
 
-    it('should simulate call and throw exception', async () => {
-      jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.reject(new Error('some error'))
-      )
+    it("should simulate call and throw exception", async () => {
+      jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.reject(new Error("some error")));
       const genderResponse = await checkIfTodayIsPublicHoliday("GB");
-      expect(genderResponse).toEqual(false)
+      expect(genderResponse).toEqual(false);
     });
-  })
+  });
 
-  describe('getNextPublicHolidays tests', () => {
+  describe("getNextPublicHolidays tests", () => {
     afterEach(() => {
       jest.clearAllMocks();
     });
 
-    it('should trigger call with expected params', async () => {
+    it("should trigger call with expected params", async () => {
       const country = "GB";
-      const axiosGetSpy = jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.resolve({ data: holidaysMock })
-      )
+      const axiosGetSpy = jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.resolve({ data: holidaysMock }));
       const genderResponse = await getNextPublicHolidays(country);
-      expect(axiosGetSpy).toHaveBeenCalledWith(`${PUBLIC_HOLIDAYS_API_URL}/NextPublicHolidays/${country}`)
+      expect(axiosGetSpy).toHaveBeenCalledWith(
+        `${PUBLIC_HOLIDAYS_API_URL}/NextPublicHolidays/${country}`,
+      );
     });
 
-    it('should simulate call and return data', async () => {
-      jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.resolve({ data: holidaysMock })
-      )
+    it("should simulate call and return data", async () => {
+      jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.resolve({ data: holidaysMock }));
       const genderResponse = await getNextPublicHolidays("GB");
-      expect(genderResponse).toEqual(holidaysMock)
+      expect(genderResponse).toEqual(holidaysMock);
     });
 
-    it('should simulate call and throw exception', async () => {
-      jest.spyOn(axios, 'get').mockImplementation(
-          () => Promise.reject(new Error('some error'))
-      )
-      const genderResponse = await getNextPublicHolidays(  "GB");
-      expect(genderResponse).toEqual([])
+    it("should simulate call and throw exception", async () => {
+      jest
+        .spyOn(axios, "get")
+        .mockImplementation(() => Promise.reject(new Error("some error")));
+      const genderResponse = await getNextPublicHolidays("GB");
+      expect(genderResponse).toEqual([]);
     });
-  })
+  });
 });
--- src/services/public-holidays.service.ts
+++ src/services/public-holidays.service.ts
@@ -1,11 +1,14 @@
-import axios from 'axios';
-import { PUBLIC_HOLIDAYS_API_URL } from '../config';
-import { validateInput, shortenPublicHoliday } from '../helpers';
-import { PublicHoliday, PublicHolidayShort } from '../types';
+import axios from "axios";
+import { PUBLIC_HOLIDAYS_API_URL } from "../config";
+import { validateInput, shortenPublicHoliday } from "../helpers";
+import { PublicHoliday, PublicHolidayShort } from "../types";
 
 // Swagger https://date.nager.at/swagger/index.html
 
-export const getListOfPublicHolidays = async (year: number, country: string): Promise<PublicHolidayShort[]> => {
+export const getListOfPublicHolidays = async (
+  year: number,
+  country: string,
+): Promise<PublicHolidayShort[]> => {
   validateInput({ year, country });
 
   try {
@@ -22,14 +25,18 @@ export const checkIfTodayIsPublicHoliday = async (country: string) => {
   validateInput({ country });
 
   try {
-    const { status } = await axios.get<PublicHoliday[]>(`${PUBLIC_HOLIDAYS_API_URL}/IsTodayPublicHoliday/${country}`);
+    const { status } = await axios.get<PublicHoliday[]>(
+      `${PUBLIC_HOLIDAYS_API_URL}/IsTodayPublicHoliday/${country}`,
+    );
     return status === 200;
   } catch (error) {
     return false;
   }
 };
 
-export const getNextPublicHolidays = async (country: string): Promise<PublicHolidayShort[]> => {
+export const getNextPublicHolidays = async (
+  country: string,
+): Promise<PublicHolidayShort[]> => {
   validateInput({ country });
 
   try {
