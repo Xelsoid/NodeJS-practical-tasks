28c568f157b911d6e2add8ce780a2f4e049b1bb8
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2023-11-06 10:46:55 +0500

    feat: integration products with DB

--- api2/repositories/user.repository.ts
+++ api2/repositories/user.repository.ts
@@ -5,5 +5,5 @@ export const findUserById = async (
   currentUserId: string,
 ): Promise<UserEntity | null> => {
   const user = await User.findByPk(currentUserId);
-  return user.id;
+  return user;
 };
